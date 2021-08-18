0d7e5e4661699bb1dca69d8d1bfa062595df8388
shaown <smshaown65@gmail.com>
2021-08-18 11:52:26 +0600

    class 5

--- pin.js
+++ pin.js
@@ -1,3 +1,17 @@
-function generatePin() {
-    console.log('generatePin ')
+
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if(pinString.length == 4) {
+        return pin;
+        
+    }else{
+        // console.log('got 3 digit and  calling again', lin);
+        return getPin();
+    }
 }
+
+function generatePin() {
+   const pin = getPin();
+   document.getElementById('display-pin').value = pin;
+} 
