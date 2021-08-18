b1ef80f6ea276ecc629d1d964866a2959d3868c5
shaown <smshaown65@gmail.com>
2021-08-18 13:19:01 +0600

    completed the pin matcher

--- pin.js
+++ pin.js
@@ -15,3 +15,35 @@ function generatePin() {
    const pin = getPin();
    document.getElementById('display-pin').value = pin;
 } 
+
+
+document.getElementById('key-pad').addEventListener('click', function(event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if(isNaN(number)){
+        if(number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
+
+
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if(pin == typedNumbers) {
+      successMessage.style.display = 'block';
+      failError.style.display = 'none';
+    }else {
+        successMessage.style.display = 'none';
+        failError.style.display = 'block';
+    }
+}
